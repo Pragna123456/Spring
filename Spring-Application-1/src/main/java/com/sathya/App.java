d7539fa9b640739b84f0d92504b9b674829a73c9
Pragna123456 <154363234+Pragna123456@users.noreply.github.com>
2023-12-29 20:48:58 +0530

    Add files via upload

---
+++ Spring-Application-1/src/main/java/com/sathya/App.java
@@ -0,0 +1,14 @@
+package com.sathya;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+	  ApplicationContext ctx=new ClassPathXmlApplicationContext("spconfig.xml");
+       StudentBean s1=(StudentBean)ctx.getBean("id1");
+       s1.printStudent();
+    }
+}
