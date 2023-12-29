a547959cf9dfb59bae43f9f6bb32750fb14f8faa
Pragna123456 <154363234+Pragna123456@users.noreply.github.com>
2023-12-29 20:51:16 +0530

    Add files via upload

---
+++ Spring-Application-6-Collectiondata2/src/main/java/com/sathya/EmployeeBean.java
@@ -0,0 +1,33 @@
+package com.sathya;
+
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeBean {
+	private int empId;
+	private List<Integer> empProjects;
+	private Map<Integer, String> empMap;
+	public EmployeeBean() {
+		super();
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public List<Integer> getEmpProjects() {
+		return empProjects;
+	}
+	public void setEmpProjects(List<Integer> empProjects) {
+		this.empProjects = empProjects;
+	}
+	public Map<Integer, String> getEmpMap() {
+		return empMap;
+	}
+	public void setEmpMap(Map<Integer, String> empMap) {
+		this.empMap = empMap;
+	}
+	
+
+}
